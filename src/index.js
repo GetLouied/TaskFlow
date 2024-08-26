2f85e94ff6f5d86e0665b5ac4eee947a7f340b8a
getlouied <derienzo_louis@yahoo.com>
2024-08-25 20:30:59 -0400

    Add initial layout

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
-import { addNewProject } from './controllers/projectController';
-import { addNewTask } from './controllers/taskController';
-
-
-let newProject = addNewProject('Project 1', 'Description', '2024-12-31');
-let newTask = addNewTask('Project 1', 'Task 1', 'Task Description', '2024-12-31', 'medium', 'Notes');
-
-console.log(newProject); 
-console.log(newTask);   
-console.log(newProject.getProjectTasks());
-console.log(newProject); 
+import './styles.css';
